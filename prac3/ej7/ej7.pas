ad53e764f2622cb04ea961939a3e443c39e460df
tomiboi01 <151480034+tomiboi01@users.noreply.github.com>
2024-04-29 19:36:07 -0300

    Update ej7.pas

--- prac3/ej7/ej7.pas
+++ prac3/ej7/ej7.pas
@@ -54,90 +54,59 @@ begin
 	close(a);
 end;
 
+procedure buscarUltimoNoBorrado(var a:archivo; pos:integer; var ultPos:integer; var ult:especieAve);
+begin
+    while (ult.cod < 0) and (ultPos > 0) do 
+    begin
+        ultPos := ultPos - 1;
+        seek(a,ultPos);
+        read(a,ult);
+    end;
+end;
+
 procedure compactar(var a:archivo);
 var cabez, ult,e : especieAve; ultPos, pos:integer;
 begin
 	reset(a);
     ultPos := filesize(a)-1;
+    seek(a,ultPos);
+    read(a,ult);
+    seek(a,0);
     pos := 0;
-    if not eof(a) then
+    leer(a,e);
+    if (e.cod <> 9999) then
     begin
-        while(pos <> ultPos) do 
+        while(pos < ultPos) do 
         begin
-            read(a,e);
-            while (pos <> ultPos) and (e.cod > 0) do 
+            while (e.cod > 0) and (pos <> ultPos) do 
             begin
                 pos := pos + 1;
                 read(a,e);
             end;
-            if (pos <> ultPos) then 
-            begin
-                writeln(pos);
-                seek(a,ultPos);
-                read(a,ult);
-            end;
-            while(pos <> ultPos) and (ult.cod > 0) do 
-            begin
-                ultPos := ultPos - 1; 
-                seek(a,ultPos);
-                read(a,ult);
-            end;
-            if (pos <> ultPos) then
+            buscarUltimoNoBorrado(a, pos,ultPos, ult);
+                
+            if (pos < ultPos) then 
             begin
                 seek(a,pos);
                 write(a,ult);
-                pos := pos + 1;
                 ultPos := ultPos - 1;
-                seek(a,pos);
+                pos := pos + 1;
+                if (pos < ultPos) then 
+                begin
+                    seek(a,ultPos);
+                    read(a,ult);
+                    seek(a,pos);
+                    read(a,e);
+                end;
             end;
         end;
-        if (ult.cod < 0) then 
-        BEGIN
+        if (ultPos = 0) then 
+            seek(a,ultPos);
+        else 
             seek(a,ultPos+1);
-            truncate(a);
-            close(a);
-        end;
-    end;
-	
-end;
-
-procedure compactar2(var a:archivo);
-var ult,e : especieAve; regBorrados, pos:integer;
-begin
-	reset(a);
-    pos := 0;
-    regBorrados := 0;
-    leer(a,e);
-    while (e.cod <> 9999) do 
-    begin
-        while (e.cod <> 9999) and (e.cod > 0) do 
-        begin
-            pos := pos + 1;
-            leer(a,e);
-            writeln(e.cod,'ay dios mio');
-        end;
-        writeln(e.cod);
-        if (e.cod <> 9999) then
-        begin
-            regBorrados := regBorrados + 1;
-            seek(a, filesize(a)-regBorrados);
-            read(a,ult);
-            while(ult.cod < 0) do 
-            begin
-                seek(a,filepos(a)-2);
-                read(a,ult);
-            end;
-            writeln(ult.cod);
-            seek(a,pos);
-            write(a,ult);
-            pos := pos + 1;
-            seek(a,pos);
-            leer(a,e);
-        end;
-        seek(a,filesize(a)-regBorrados);
         truncate(a);
     end;
-    
+    close(a);
 end;
 
 procedure informar(var a:archivo);
@@ -178,7 +147,7 @@ BEGIN
 	informar(maestro);
 
     readln(e.cod);
-	compactar2(maestro);
+	compactar(maestro);
 
     informar(maestro);
 end.
